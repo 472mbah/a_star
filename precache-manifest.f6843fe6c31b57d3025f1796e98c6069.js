fd432a6f986fd30fd7fa08207dea37088db4cc31
Momodou Bah <472mbah@gmail.com>
2020-08-26 15:06:41 +0100

    Updates

---
+++ precache-manifest.f6843fe6c31b57d3025f1796e98c6069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf04df2f78fc2498b0b4b873a610d60",
+    "url": "/a_star/index.html"
+  },
+  {
+    "revision": "9cfdafb2299cb9ceb80b",
+    "url": "/a_star/static/css/main.25a53d4f.chunk.css"
+  },
+  {
+    "revision": "f92b2b55caf5765a821e",
+    "url": "/a_star/static/js/2.426ab51e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a_star/static/js/2.426ab51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfdafb2299cb9ceb80b",
+    "url": "/a_star/static/js/main.d5c279ef.chunk.js"
+  },
+  {
+    "revision": "15e0ff3e5031448b732c",
+    "url": "/a_star/static/js/runtime-main.087e94f8.js"
+  }
+]);
